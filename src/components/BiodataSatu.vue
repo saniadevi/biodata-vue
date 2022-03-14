114a85ef3d573651942cb8edfdfb8df1569a10c5
saniadevi <fzats44@gmail.com>
2022-03-14 11:18:27 +0700

    first commit

---
+++ src/components/BiodataSatu.vue
@@ -0,0 +1,217 @@
+<template>
+  <div id="app">
+    <div class="cv2" style="padding: 40px">
+      <div class="head">
+        <fieldset class="resume"><legend>&ensp;RESUME&ensp;</legend></fieldset>
+        <h1
+          style="
+            font-family: times new roman;
+            text-align: left;
+            color: white;
+            margin-left: 390px;
+          "
+        >
+          FAZA <br /><span style="color: black"> TSANIA DEVI</span>
+        </h1>
+      </div>
+
+      <h2 style="text-align: center; width: 1020px">PERSONAL DATA</h2>
+      <hr style="border: 1px solid black; width: 840px; float: right" />
+
+      <div class="border3">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTz6BzRt-Yvzs7_mCh_ipkJVyTsEgFskdpmVDIGJJoVhf__9ODVFBXeKkwkfYFTnSwFmOk&usqp=CAU"
+          style="
+            border: 10px solid white;
+            border-radius: 100%;
+            width: 160px;
+            height: 160px;
+            margin-top: -180px;
+          "
+        />
+
+        <h2>PROFILE</h2>
+        <hr style="border: 1px dashed" />
+        <p><b>NAME</b></p>
+        <p>Faza Tsania Devi</p>
+        <p><b>DATE OF BIRTH</b></p>
+        <p>04 April 2006</p>
+        <p><b>ADDRESS</b></p>
+        <p>Mendiro, East Ungaran, Semarang Regency</p>
+        <p><b>WEBSITE</b></p>
+        <p>saniadv.com</p>
+        <h2>AWARDS</h2>
+        <hr style="border: 1px dashed" />
+        <p><b>CHARTER AWARD 2012 </b></p>
+        <p>Sniacv.com</p>
+        <p><b>INTERFACE DESIGN 2013 </b></p>
+        <p>Devids.com</p>
+
+        <h2>CONTACT</h2>
+        <hr style="border: 1px dashed" />
+        <p><b> HOME NUMBER &#9742;</b><br />(0411) 123 667</p>
+        <p></p>
+        <p><b>PHONE NUMBER &#x1F4DE;</b><br />+62 882 0031 84981</p>
+        <p></p>
+        <p><b>EMAIL &#x1F4E7;</b><br />fzats44@gmail.com</p>
+        <p>
+          <u></u>
+        </p>
+
+        <h2>SOCIAL</h2>
+        <hr style="border: 1px dashed" />
+        <p><b>TWITTER</b></p>
+        <p>@Azafnia</p>
+        <p><b>FACEBOOOK </b></p>
+        <p>Azaf_Nia</p>
+      </div>
+
+      <div class="border4">
+        <div id="p">
+          <h2>&#x1F550; WORDS OF WISDOM</h2>
+          <hr style="border: 1px dashed" />
+          <div class="wr">
+            <h3>SUNDAY, 1 OCTOBER 2021</h3>
+            <p>
+              My favorite books have a personality and complexion as distinctly
+              drawn as if the author's portrait were framed into the paragraphs
+              and smiled upon me as I read his illustrated pages.
+            </p>
+          </div>
+          <div class="wr">
+            <h3>MONDAY, 2 OCTOBER 2021</h3>
+            <p>
+              It has always been my practice to cast a long paragraph in a
+              single mould, to try it by my ear, to deposit it in my memory, but
+              to suspend the action of the pen till I had given the last polish
+              to my work.
+            </p>
+          </div>
+          <div class="wr">
+            <h3>TUESDAY, 3 OCTOBER 2021</h3>
+            <p>
+              The essence of life is not what we know, not the books we read or
+              the sentences we speak, but what we do, what is most deeply rooted
+              in our hearts, souls and the core of our lives.
+            </p>
+          </div>
+        </div>
+        <br />
+        <div id="p">
+          <h2>ACHIEVEMENT &#x1F4DD;</h2>
+          <hr style="border: 1px dashed" />
+          <div class="ac">
+            <h3>COMPETITION 2016</h3>
+            <p>take part in Qira' competition</p>
+          </div>
+          <div class="ac">
+            <h3>COMPETITION 2016</h3>
+            <p>got a class 1 champion certificate</p>
+          </div>
+          <div class="ac">
+            <h3>COMPETITION 2018</h3>
+            <p>take part in bootcamp activities</p>
+          </div>
+        </div>
+        <br />
+        <div id="p">
+          <h2>&#x1F4A1; SKILLS</h2>
+          <hr style="border: 1px dashed" />
+          <div class="skil">
+            <h3>PROFESSIONAL SKILLS</h3>
+            <p>Coding&nbsp; &#9899; &#9899; &#9899;</p>
+            <p>Singing &nbsp;&nbsp; &#9899; &#9899; &#9899;</p>
+            <p>organization&nbsp; &#9899; &#9899; &#9899;</p>
+          </div>
+
+          <div class="skil">
+            <h3>PERSONAL SKILLS</h3>
+            <p>mutual cooperation&nbsp; &#9899; &#9899; &#9899;</p>
+            <p>mutual help&nbsp; &#9899; &#9899; &#9899;</p>
+            <p>friendly&nbsp; &#9899; &#9899; &#9899;</p>
+          </div>
+        </div>
+
+        <br />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.resume {
+  border-top: 10px solid white;
+  color: white;
+  border-left: none;
+  border-bottom: none;
+  border-right: none;
+  margin-bottom: 10px;
+}
+/* .border1 {
+  border: 1px solid lightgrey;
+  padding: 20px;
+  width: 300px;
+  background-color: lightgrey;
+  float: left;
+}
+.border2 {
+  border: 1px solid white;
+  padding: 20px;
+  background-color: white;
+  float: left;
+  width: 60%;
+  height: 894px;
+  margin-bottom: 50px;
+} */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+.head {
+  background-color: grey;
+  padding: 20px;
+}
+.border3 {
+  border: 1px solid black none;
+  padding: 20px;
+  width: 350px;
+  background-color: white;
+  float: left;
+  text-align: right;
+}
+.border4 {
+  border: 1px solid black none;
+  padding: 20px;
+  float: left;
+  width: 65%;
+  height: 894px;
+  margin-bottom: 50px;
+}
+#p {
+  background-color: lightgrey;
+  padding: 20px;
+  height: 320px;
+}
+h2 {
+  font-family: "Times New Roman", Times, serif;
+}
+.wr {
+  float: left;
+  width: 245px;
+  height: 300px;
+  padding: 5px;
+  /* border: 1px solid red; */
+}
+.ac {
+  float: left;
+  width: 30%;
+  height: 300px;
+  padding: 5px;
+}
+.skil {
+  float: left;
+  width: 40%;
+  height: 300px;
+}
+</style>
